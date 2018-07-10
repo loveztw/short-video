6c002d80c08f663317416e26a1211adb00832dfb
Ray Zhou <rayzhou.cn@hotmail.com>
2018-07-10 23:56:19 +0800

    developing-cam

--- short-video-minipg/pages/upload/upload.js
+++ short-video-minipg/pages/upload/upload.js
@@ -1,4 +1,16 @@
 
 Page({
+  onShow: function(){
+    wx.hideTabBar({})
+  },
 
+  onUnload: function(){
+    wx.showTabBar({})
+  },
+
+  toIndex: function(){
+    wx.reLaunch({
+      url: '../index/index',
+    })
+  }
 })
