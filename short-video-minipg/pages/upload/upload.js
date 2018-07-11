a7c5d94cad32a5d99b4ed4bb7906c4565ff31e00
Ray Zhou <rayzhou.cn@hotmail.com>
2018-07-11 23:30:46 +0800

    developing at home

--- short-video-minipg/pages/upload/upload.js
+++ short-video-minipg/pages/upload/upload.js
@@ -1,5 +1,11 @@
 
 Page({
+  data: {
+    recordFlg: false,
+    devicePosOpts: ['front', 'back'],
+    devicePosIndex: 0,
+  },
+
   onShow: function(){
     wx.hideTabBar({})
   },
@@ -12,5 +18,25 @@ Page({
     wx.reLaunch({
       url: '../index/index',
     })
+  },
+
+  startRecord: function(){
+    this.setData({
+      recordFlg: true,
+    })
+  },
+
+  endRecord: function(){
+    this.setData({
+      recordFlg: false,
+    })
+  },
+
+  doReverse: function(){
+    if (this.data.devicePosIndex == 0) {
+      this.data.devicePosIndex = 1;
+    } else {
+      this.data.devicePosIndex = 0;
+    }
   }
 })
