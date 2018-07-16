363ae41afb98b7f7ed445d38d6caf7a83b5c2406
Ray Zhou <rayzhou.cn@hotmail.com>
2018-07-16 23:19:37 +0800

    developing at home: pull up load

--- short-video-minipg/pages/music_main/music_main.js
+++ short-video-minipg/pages/music_main/music_main.js
@@ -5,6 +5,8 @@ Page({
     showMenuFlg: "",
     bgmList: [],
     serverUrl: app.serverUrl,
+    curPage: 0,
+    totalPage: 0,
   },
 
   showMenu: function(e) {
@@ -14,18 +16,50 @@ Page({
     })
   },
 
-  onShow: function() {
+  onLoad: function() {
     var serverUrl = app.serverUrl;  
     var that = this;
+
+    wx.showLoading({
+      title: '加载中',
+    })
     wx.request({
-      url: serverUrl + '/bgm/list/1',
-      method: 'GET',
-      header: {
-        'content-type': 'application/json' //默认值
-      },
+      url: serverUrl + '/bgm/listPage?page=1',
+      method: 'POST',
       success: function (res) {
+        wx.hideLoading();
         that.setData({
-          bgmList: res.data.data,
+          bgmList: res.data.data.rows,
+          curPage: res.data.data.page,
+          totalPage: res.data.data.total,
+        })
+      }
+    })
+  },
+
+  //上拉刷新
+  onReachBottom: function() {
+    var curPage = this.data.curPage;
+
+    if (curPage >= this.data.totalPage) {
+      return;
+    }
+
+    var newBgmList = this.data.bgmList;
+    this.setData({
+      curPage: curPage + 1,
+      bgmList: [],
+    })
+    curPage = this.data.curPage;
+    var serverUrl = app.serverUrl;
+    var that = this;
+    wx.request({
+      url: serverUrl + '/bgm/listPage?page=' + curPage,
+      method: 'POST',
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          bgmList: newBgmList.concat(res.data.data.rows),
         })
       }
     })
--- short-videos-dev/short-videos-dev-mini-api/src/main/java/com/miyo/controller/bgmController.java
+++ short-videos-dev/short-videos-dev-mini-api/src/main/java/com/miyo/controller/bgmController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miyo.pojo.vo.BgmVO;
@@ -28,8 +28,8 @@ public class bgmController {
 		return JsonResponse.ok(bgmVOList);
 	}
 
-	@PostMapping("/listPage")
-	public JsonResponse getBgmListPage(@RequestBody Integer page) {
+	@PostMapping(value="/listPage")
+	public JsonResponse getBgmListPage(@RequestParam Integer page) {
 		
 		PagedResult pagedResult = bgmService.getBgmByPage(page);
 		return JsonResponse.ok(pagedResult);
